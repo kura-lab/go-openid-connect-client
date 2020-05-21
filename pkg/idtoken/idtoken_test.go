a2524df6332ec831f7b5bb7d35d3bb128de6807b
kura <mkurahay@gmail.com>
2020-05-22 03:03:56 +0900

    added VerifySignature(RS384 and RS512) tests.

--- pkg/idtoken/idtoken_test.go
+++ pkg/idtoken/idtoken_test.go
@@ -16,91 +16,100 @@ import (
 
 func TestNewOIDCConfigSuccess(t *testing.T) {
 
-	header := map[string]string{
-		"typ": "JWT",
-		"alg": "RS256",
-		"kid": "KEY_ID",
-	}
-	jsonHeader, err := json.Marshal(header)
-	encodedHeader := base64.RawURLEncoding.EncodeToString(jsonHeader)
-
-	payload := map[string]string{
-		"sub": "123456789",
-	}
-	jsonPayload, err := json.Marshal(payload)
-	encodedPayload := base64.RawURLEncoding.EncodeToString(jsonPayload)
-
-	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
-
-	publicKey := privateKey.PublicKey
-	modulus := base64.RawURLEncoding.EncodeToString(publicKey.N.Bytes())
-	exponent := base64.RawURLEncoding.EncodeToString(big.NewInt(int64(publicKey.E)).Bytes())
-
-	data := encodedHeader + "." + encodedPayload
-
-	hash := crypto.Hash.New(crypto.SHA256)
-	hash.Write(([]byte)(data))
-	hashed := hash.Sum(nil)
-
-	signature, _ := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed)
-
-	encodedSignature := base64.RawURLEncoding.EncodeToString(signature)
-
-	rawIDToken := strings.Join(
-		[]string{
-			encodedHeader,
-			encodedPayload,
-			encodedSignature,
-		},
-		".",
-	)
-
-	config := oidcconfig.NewOIDCConfig(
-		oidcconfig.Issuer("https://op.example.com"),
-		oidcconfig.IDTokenSigningAlgValuesSupported([]string{"RS256"}),
-	)
-	oIDCConfigResponse := config.Response()
-
-	iDTokenPointer, err := NewIDToken(
-		oIDCConfigResponse,
-		rawIDToken,
-	)
-
-	if err != nil {
-		t.Fatalf("failed to decode id token: %#v", err)
+	algorithms := [][]interface{}{
+		{"RS256", crypto.SHA256},
+		{"RS384", crypto.SHA384},
+		{"RS512", crypto.SHA512},
 	}
 
-	if err := iDTokenPointer.VerifyIDTokenHeader(); err != nil {
-		t.Fatalf("invalid claim in id token header: %#v", err)
-	}
+	for _, algorithm := range algorithms {
 
-	iDTokenPointerHeader := iDTokenPointer.GetIDTokenHeader()
+		header := map[string]string{
+			"typ": "JWT",
+			"alg": algorithm[0].(string),
+			"kid": "KEY_ID",
+		}
+		jsonHeader, err := json.Marshal(header)
+		encodedHeader := base64.RawURLEncoding.EncodeToString(jsonHeader)
 
-	if iDTokenPointerHeader.Type != "JWT" {
-		t.Fatalf("invalid typ. expected: JWT, actual: %v", iDTokenPointerHeader.Type)
-	}
-	if iDTokenPointerHeader.Algorithm != "RS256" {
-		t.Fatalf("invalid alg. expected: RS256, actual: %v", iDTokenPointerHeader.Algorithm)
-	}
-	if iDTokenPointerHeader.KeyID != "KEY_ID" {
-		t.Fatalf("invalid kid. expected: KEY_ID, actual: %v", iDTokenPointerHeader.KeyID)
-	}
+		payload := map[string]string{
+			"sub": "123456789",
+		}
+		jsonPayload, err := json.Marshal(payload)
+		encodedPayload := base64.RawURLEncoding.EncodeToString(jsonPayload)
 
-	jWKsResponse := jwks.Response{
-		KeySets: []jwks.KeySet{
-			{
-				KeyID:     "KEY_ID",
-				KeyType:   "RSA",
-				Algorithm: "RS256",
-				Use:       "sig",
-				Modulus:   modulus,
-				Exponent:  exponent,
+		privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+		publicKey := privateKey.PublicKey
+		modulus := base64.RawURLEncoding.EncodeToString(publicKey.N.Bytes())
+		exponent := base64.RawURLEncoding.EncodeToString(big.NewInt(int64(publicKey.E)).Bytes())
+
+		data := encodedHeader + "." + encodedPayload
+
+		hash := crypto.Hash.New(algorithm[1].(crypto.Hash))
+		hash.Write(([]byte)(data))
+		hashed := hash.Sum(nil)
+
+		signature, _ := rsa.SignPKCS1v15(rand.Reader, privateKey, algorithm[1].(crypto.Hash), hashed)
+
+		encodedSignature := base64.RawURLEncoding.EncodeToString(signature)
+
+		rawIDToken := strings.Join(
+			[]string{
+				encodedHeader,
+				encodedPayload,
+				encodedSignature,
 			},
-		},
-	}
+			".",
+		)
 
-	if err := iDTokenPointer.VerifySignature(jWKsResponse); err != nil {
-		t.Fatalf("invalid signature: expected: true")
+		config := oidcconfig.NewOIDCConfig(
+			oidcconfig.Issuer("https://op.example.com"),
+			oidcconfig.IDTokenSigningAlgValuesSupported([]string{algorithm[0].(string)}),
+		)
+		oIDCConfigResponse := config.Response()
+
+		iDTokenPointer, err := NewIDToken(
+			oIDCConfigResponse,
+			rawIDToken,
+		)
+
+		if err != nil {
+			t.Fatalf("failed to decode id token: %#v", err)
+		}
+
+		if err := iDTokenPointer.VerifyIDTokenHeader(); err != nil {
+			t.Fatalf("invalid claim in id token header: %#v", err)
+		}
+
+		iDTokenPointerHeader := iDTokenPointer.GetIDTokenHeader()
+
+		if iDTokenPointerHeader.Type != "JWT" {
+			t.Fatalf("invalid typ. expected: JWT, actual: %v", iDTokenPointerHeader.Type)
+		}
+		if iDTokenPointerHeader.Algorithm != algorithm[0].(string) {
+			t.Fatalf("invalid alg. expected: %v, actual: %v", algorithm[0].(string), iDTokenPointerHeader.Algorithm)
+		}
+		if iDTokenPointerHeader.KeyID != "KEY_ID" {
+			t.Fatalf("invalid kid. expected: KEY_ID, actual: %v", iDTokenPointerHeader.KeyID)
+		}
+
+		jWKsResponse := jwks.Response{
+			KeySets: []jwks.KeySet{
+				{
+					KeyID:     "KEY_ID",
+					KeyType:   "RSA",
+					Algorithm: algorithm[0].(string),
+					Use:       "sig",
+					Modulus:   modulus,
+					Exponent:  exponent,
+				},
+			},
+		}
+
+		if err := iDTokenPointer.VerifySignature(jWKsResponse); err != nil {
+			t.Fatalf("invalid signature: expected: true")
+		}
 	}
 }
 
