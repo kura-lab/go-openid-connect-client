8cbcf88a2ba25a3b6037645f03b31f47fc23cf38
kura <mkurahay@gmail.com>
2020-05-26 02:08:48 +0900

    fixed decoding and unmashaling tests.

--- pkg/idtoken/idtoken_new_test.go
+++ pkg/idtoken/idtoken_new_test.go
@@ -58,85 +58,96 @@ func TestNewOIDCConfigSuccess(t *testing.T) {
 
 func TestNewOIDCConfigDecodeHeaderFailure(t *testing.T) {
 
-	encodedHeader := "INVALID_HEADER"
-
-	payload := map[string]string{
-		"sub": "123456789",
-	}
-	jsonPayload, err := json.Marshal(payload)
-	encodedPayload := base64.RawURLEncoding.EncodeToString(jsonPayload)
-
-	signature := base64.RawURLEncoding.EncodeToString([]byte("SIGNATURE"))
-
-	rawIDToken := strings.Join(
-		[]string{
-			encodedHeader,
-			encodedPayload,
-			signature,
-		},
-		".",
-	)
-
-	config := oidcconfig.NewOIDCConfig(
-		oidcconfig.Issuer("https://op.example.com"),
-		oidcconfig.IDTokenSigningAlgValuesSupported([]string{"RS256"}),
-	)
-	oIDCConfigResponse := config.Response()
-
-	iDTokenPointer, err := NewIDToken(
-		oIDCConfigResponse,
-		rawIDToken,
-	)
-
-	if err == nil {
-		t.Fatalf("succuess to new id token")
+	data := []string{
+		"INVALID_HEADER_DECODE_ERROR!!!",
+		"INVALID_HEADER_JSON_UNMARSHAL_ERROR",
 	}
 
-	if iDTokenPointer != nil {
-		t.Fatalf("unexpected return: %#v", iDTokenPointer)
+	for _, header := range data {
+
+		payload := map[string]string{
+			"sub": "123456789",
+		}
+		jsonPayload, err := json.Marshal(payload)
+		encodedPayload := base64.RawURLEncoding.EncodeToString(jsonPayload)
+
+		signature := base64.RawURLEncoding.EncodeToString([]byte("SIGNATURE"))
+
+		rawIDToken := strings.Join(
+			[]string{
+				header,
+				encodedPayload,
+				signature,
+			},
+			".",
+		)
+
+		config := oidcconfig.NewOIDCConfig(
+			oidcconfig.Issuer("https://op.example.com"),
+			oidcconfig.IDTokenSigningAlgValuesSupported([]string{"RS256"}),
+		)
+		oIDCConfigResponse := config.Response()
+
+		iDTokenPointer, err := NewIDToken(
+			oIDCConfigResponse,
+			rawIDToken,
+		)
+
+		if err == nil {
+			t.Fatalf("succuess to new id token")
+		}
+
+		if iDTokenPointer != nil {
+			t.Fatalf("unexpected return: %#v", iDTokenPointer)
+		}
 	}
 }
 
 func TestNewOIDCConfigDecodePayloadFailure(t *testing.T) {
 
-	header := map[string]string{
-		"typ": "JWT",
-		"alg": "RS256",
-		"kid": "KEY_ID",
-	}
-	jsonHeader, err := json.Marshal(header)
-	encodedHeader := base64.RawURLEncoding.EncodeToString(jsonHeader)
-
-	encodedPayload := "INVALID_PAYLOAD"
-
-	signature := base64.RawURLEncoding.EncodeToString([]byte("SIGNATURE"))
-
-	rawIDToken := strings.Join(
-		[]string{
-			encodedHeader,
-			encodedPayload,
-			signature,
-		},
-		".",
-	)
-
-	config := oidcconfig.NewOIDCConfig(
-		oidcconfig.Issuer("https://op.example.com"),
-		oidcconfig.IDTokenSigningAlgValuesSupported([]string{"RS256"}),
-	)
-	oIDCConfigResponse := config.Response()
-
-	iDTokenPointer, err := NewIDToken(
-		oIDCConfigResponse,
-		rawIDToken,
-	)
-
-	if err == nil {
-		t.Fatalf("succuess to new id token")
+	data := []string{
+		"INVALID_PAYLOAD_DECODE_ERROR!!!",
+		"INVALID_PAYLOAD_JSON_UNMARSHAL_ERROR",
 	}
 
-	if iDTokenPointer != nil {
-		t.Fatalf("unexpected return: %#v", iDTokenPointer)
+	for _, payload := range data {
+		header := map[string]string{
+			"typ": "JWT",
+			"alg": "RS256",
+			"kid": "KEY_ID",
+		}
+		jsonHeader, err := json.Marshal(header)
+		encodedHeader := base64.RawURLEncoding.EncodeToString(jsonHeader)
+
+		signature := base64.RawURLEncoding.EncodeToString([]byte("SIGNATURE"))
+
+		rawIDToken := strings.Join(
+			[]string{
+				encodedHeader,
+				payload,
+				signature,
+			},
+			".",
+		)
+
+		config := oidcconfig.NewOIDCConfig(
+			oidcconfig.Issuer("https://op.example.com"),
+			oidcconfig.IDTokenSigningAlgValuesSupported([]string{"RS256"}),
+		)
+		oIDCConfigResponse := config.Response()
+
+		iDTokenPointer, err := NewIDToken(
+			oIDCConfigResponse,
+			rawIDToken,
+		)
+
+		if err == nil {
+			t.Fatalf("succuess to new id token")
+		}
+
+		if iDTokenPointer != nil {
+			t.Fatalf("unexpected return: %#v", iDTokenPointer)
+		}
 	}
 }
 
@@ -158,6 +169,53 @@ func TestNewOIDCConfigDecodePayloadAudienceFailure(t *testing.T) {
 	encodedPayload := base64.RawURLEncoding.EncodeToString(jsonPayload)
 
 	signature := base64.RawURLEncoding.EncodeToString([]byte("SIGNATURE"))
+
+	rawIDToken := strings.Join(
+		[]string{
+			encodedHeader,
+			encodedPayload,
+			signature,
+		},
+		".",
+	)
+
+	config := oidcconfig.NewOIDCConfig(
+		oidcconfig.Issuer("https://op.example.com"),
+		oidcconfig.IDTokenSigningAlgValuesSupported([]string{"RS256"}),
+	)
+	oIDCConfigResponse := config.Response()
+
+	iDTokenPointer, err := NewIDToken(
+		oIDCConfigResponse,
+		rawIDToken,
+	)
+
+	if err == nil {
+		t.Fatalf("succuess to new id token")
+	}
+
+	if iDTokenPointer != nil {
+		t.Fatalf("unexpected return: %#v", iDTokenPointer)
+	}
+}
+
+func TestNewOIDCConfigDecodeSignatureFailure(t *testing.T) {
+
+	header := map[string]string{
+		"typ": "JWT",
+		"alg": "RS256",
+		"kid": "KEY_ID",
+	}
+	jsonHeader, err := json.Marshal(header)
+	encodedHeader := base64.RawURLEncoding.EncodeToString(jsonHeader)
+
+	payload := map[string]interface{}{
+		"sub": "123456789",
+	}
+	jsonPayload, err := json.Marshal(payload)
+	encodedPayload := base64.RawURLEncoding.EncodeToString(jsonPayload)
+
+	signature := "INVALID_SIGNATURE_DECODE_ERROR!!!"
 
 	rawIDToken := strings.Join(
 		[]string{
