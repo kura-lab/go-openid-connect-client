104189c77723de69a4810aad132ccc05fdea4b28
kura <mkurahay@gmail.com>
2020-05-27 00:25:03 +0900

    added generateECDSAPublicKey error tests.

--- pkg/idtoken/idtoken_ecdsa_test.go
+++ pkg/idtoken/idtoken_ecdsa_test.go
@@ -142,5 +142,229 @@ func TestNewOIDCConfigECDSASuccess(t *testing.T) {
 	}
 }
 
-func TestNewOIDCConfigECDSAFailure(t *testing.T) {
+func TestNewOIDCConfigGenerateECDSAPublicKeyFailures(t *testing.T) {
+
+	header := map[string]string{
+		"typ": "JWT",
+		"alg": "ES256",
+		"kid": "KEY_ID",
+	}
+	jsonHeader, _ := json.Marshal(header)
+	encodedHeader := base64.RawURLEncoding.EncodeToString(jsonHeader)
+
+	payload := map[string]string{
+		"sub": "123456789",
+	}
+	jsonPayload, _ := json.Marshal(payload)
+	encodedPayload := base64.RawURLEncoding.EncodeToString(jsonPayload)
+
+	ellipticCurve := elliptic.P256()
+
+	privateKey, _ := ecdsa.GenerateKey(ellipticCurve, rand.Reader)
+
+	publicKey := privateKey.PublicKey
+
+	hash := crypto.SHA256.New()
+	r := big.NewInt(0)
+	s := big.NewInt(0)
+
+	data := encodedHeader + "." + encodedPayload
+
+	hash.Write(([]byte)(data))
+	hashed := hash.Sum(nil)
+
+	r, s, _ = ecdsa.Sign(rand.Reader, privateKey, hashed)
+
+	keySize := privateKey.Curve.Params().BitSize / 8
+	if privateKey.Curve.Params().BitSize%8 > 0 {
+		keySize++
+	}
+
+	var signature []byte
+	rPad := keySize - len(r.Bytes())
+	if rPad > 0 {
+		zeroPad := make([]byte, rPad)
+		signature = append(zeroPad, r.Bytes()...)
+	} else {
+		signature = append(signature, r.Bytes()...)
+	}
+
+	sPad := keySize - len(s.Bytes())
+	if sPad > 0 {
+		zeroPad := make([]byte, sPad)
+		signature = append(signature, zeroPad...)
+		signature = append(signature, s.Bytes()...)
+	} else {
+		signature = append(signature, s.Bytes()...)
+	}
+
+	encodedSignature := base64.RawURLEncoding.EncodeToString(signature)
+
+	rawIDToken := strings.Join(
+		[]string{
+			encodedHeader,
+			encodedPayload,
+			encodedSignature,
+		},
+		".",
+	)
+
+	config := oidcconfig.NewOIDCConfig(
+		oidcconfig.Issuer("https://op.example.com"),
+		oidcconfig.IDTokenSigningAlgValuesSupported([]string{"ES256"}),
+	)
+	oIDCConfigResponse := config.Response()
+
+	iDTokenPointer, _ := NewIDToken(
+		oIDCConfigResponse,
+		rawIDToken,
+	)
+
+	if err := iDTokenPointer.VerifyIDTokenHeader(); err != nil {
+		t.Fatalf("invalid claim in id token header: %#v", err)
+	}
+
+	jWKsResponse := jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "EC",
+				Algorithm:   "ES256",
+				Use:         "INVALID_USE",
+				Curve:       "P-256",
+				XCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.X.Bytes()),
+				YCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.Y.Bytes()),
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by invalid use of key set")
+	}
+
+	jWKsResponse = jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "INVALID_KEY_TYPE",
+				Algorithm:   "ES256",
+				Use:         "sig",
+				Curve:       "P-256",
+				XCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.X.Bytes()),
+				YCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.Y.Bytes()),
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by invalid kty of key set")
+	}
+
+	jWKsResponse = jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "EC",
+				Algorithm:   "UNMATCHED_ALGORITHM",
+				Use:         "sig",
+				Curve:       "P-256",
+				XCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.X.Bytes()),
+				YCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.Y.Bytes()),
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by unmatched alg of key set")
+	}
+
+	jWKsResponse = jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "EC",
+				Algorithm:   "ES256",
+				Use:         "sig",
+				Curve:       "P-256",
+				XCoordinate: "",
+				YCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.Y.Bytes()),
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by not existing x coordinate of key set")
+	}
+
+	jWKsResponse = jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "EC",
+				Algorithm:   "ES256",
+				Use:         "sig",
+				Curve:       "P-256",
+				XCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.X.Bytes()),
+				YCoordinate: "",
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by not existing y coordinate of key set")
+	}
+
+	jWKsResponse = jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "EC",
+				Algorithm:   "ES256",
+				Use:         "sig",
+				Curve:       "P-256",
+				XCoordinate: "INVALID_X_COORDIVATE!!!",
+				YCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.Y.Bytes()),
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by invalid x coordinate of key set")
+	}
+
+	jWKsResponse = jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "EC",
+				Algorithm:   "ES256",
+				Use:         "sig",
+				Curve:       "P-256",
+				XCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.X.Bytes()),
+				YCoordinate: "INVALID_Y_COORDINATE!!!",
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by invalid y coordinate of key set")
+	}
+
+	jWKsResponse = jwks.Response{
+		KeySets: []jwks.KeySet{
+			{
+				KeyID:       "KEY_ID",
+				KeyType:     "EC",
+				Algorithm:   "ES256",
+				Use:         "sig",
+				Curve:       "INVALID_CURVE_ALGORITHM",
+				XCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.X.Bytes()),
+				YCoordinate: base64.RawURLEncoding.EncodeToString(publicKey.Y.Bytes()),
+			},
+		},
+	}
+
+	if err := iDTokenPointer.VerifySignature(jWKsResponse); err == nil {
+		t.Fatalf("expect error caused by invalid curve algorithm of key set")
+	}
 }
