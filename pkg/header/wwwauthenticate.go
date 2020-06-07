d3a716a7db9929981dd3a58fd24e933d0f60d018
kura <mkurahay@gmail.com>
2020-06-08 00:01:47 +0900

    moved parse WWW-Authenticate to new header package.

---
+++ pkg/header/wwwauthenticate.go
@@ -0,0 +1,40 @@
+package header
+
+import (
+	"regexp"
+	"strings"
+)
+
+// WWWAuthenticate is struct for WWW-Authenticate header.
+type WWWAuthenticate struct {
+	Realm            string
+	Scope            string
+	Error            string
+	ErrorDescription string
+}
+
+func ParseWWWAuthenticateHeader(header string) map[string]string {
+
+	rep := regexp.MustCompile(`\ABearer `)
+	if !rep.MatchString(header) {
+		return map[string]string{}
+	}
+
+	header = rep.ReplaceAllString(header, "")
+
+	header = strings.NewReplacer(
+		"\r\n", "",
+		"\r", "",
+		"\n", "",
+	).Replace(header)
+
+	attributes := strings.Split(header, ",")
+
+	parsed := map[string]string{}
+	for _, attribute := range attributes {
+		splited := strings.Split(strings.TrimSpace(attribute), "=")
+		parsed[splited[0]] = strings.Trim(splited[1], "\"")
+	}
+
+	return parsed
+}
---
+++ pkg/header/wwwauthenticate_test.go
@@ -0,0 +1,49 @@
+package header
+
+import (
+	"testing"
+)
+
+func TestParseWWWAuthenticateHeader(t *testing.T) {
+
+	data := [][]interface{}{
+		{
+			"Bearer realm=example",
+			map[string]string{"realm": "example"},
+		},
+		{
+			"Bearer realm=\"example\",\r\nerror=\"invalid_token\"",
+			map[string]string{"realm": "example", "error": "invalid_token"},
+		},
+		{
+			"Bearer realm=\"example\",\r\nscope=\"openid profile email\",\r\nerror=\"invalid_token\",\r\nerror_description=\"The access token expired\"",
+			map[string]string{"realm": "example", "scope": "openid profile email", "error": "invalid_token", "error_description": "The access token expired"},
+		},
+		{
+			"Bearer realm=\"example\",\r\nerror=\"invalid_token\",\r\nerror_description=\"The access token expired\"",
+			map[string]string{"realm": "example", "error": "invalid_token", "error_description": "The access token expired"},
+		},
+		{
+			"Bearer realm=\"example\",\rerror=\"invalid_token\",\rerror_description=\"The access token expired\"",
+			map[string]string{"realm": "example", "error": "invalid_token", "error_description": "The access token expired"},
+		},
+		{
+			"Bearer realm=\"example\",\nerror=\"invalid_token\",\nerror_description=\"The access token expired\"",
+			map[string]string{"realm": "example", "error": "invalid_token", "error_description": "The access token expired"},
+		},
+	}
+
+	for _, value := range data {
+		parsed := ParseWWWAuthenticateHeader(string(value[0].(string)))
+
+		if len(value[1].(map[string]string)) != len(parsed) {
+			t.Errorf("not match length of parsed value. expected:%v, actual:%v", value[1], parsed)
+		}
+
+		for k, v := range value[1].(map[string]string) {
+			if v != parsed[k] {
+				t.Errorf("error. key:%s, expected:%s, actual:%s", k, v, parsed[k])
+			}
+		}
+	}
+}
