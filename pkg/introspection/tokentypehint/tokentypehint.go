0591562ae9c88eddc599f7686e6571354be5dad0
kura <mkurahay@gmail.com>
2020-06-16 00:01:23 +0900

    fixed a comment.

--- pkg/introspection/tokentypehint/tokentypehint.go
+++ pkg/introspection/tokentypehint/tokentypehint.go
@@ -1,6 +1,6 @@
 package tokentypehint
 
-// Constants for tokentypehint parameter of OAuth 2.0 Token Revocation Request.
+// Constants for tokentypehint parameter of OAuth 2.0 Token Introspection Request.
 const (
 	AccessToken  = "access_token"
 	RefreshToken = "refresh_token"
