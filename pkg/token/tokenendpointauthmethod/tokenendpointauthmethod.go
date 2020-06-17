e809a674eca2370e7aa1787940094b2f16a92380
kura <mkurahay@gmail.com>
2020-06-18 00:01:45 +0900

    added constants token endpoint auth method.

---
+++ pkg/token/tokenendpointauthmethod/tokenendpointauthmethod.go
@@ -0,0 +1,7 @@
+package tokenendpointauthmethod
+
+// Constants for token_endpoint_auth_method parameter of Token Request.
+const (
+	ClientSecretBasic = "client_secret_basic"
+	ClientSecretPost  = "client_secret_post"
+)
