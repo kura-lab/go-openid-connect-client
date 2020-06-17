39fdc6936183b11fb133a2bd692f862dffefafa1
kura <mkurahay@gmail.com>
2020-06-18 00:29:35 +0900

    deleted tokenendpointauthmethod.go.

--- pkg/token/tokenendpointauthmethod/tokenendpointauthmethod.go
+++
@@ -1,7 +0,0 @@
-package tokenendpointauthmethod
-
-// Constants for token_endpoint_auth_method parameter of Token Request.
-const (
-	ClientSecretBasic = "client_secret_basic"
-	ClientSecretPost  = "client_secret_post"
-)
